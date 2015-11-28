c2746cc6955c82d73c14bc22567936196594f679
Fe <fe.tisch88@gmail.com>
2015-11-28 14:17:43 +0100

    setup project

---
+++ src/services/InputService.java
@@ -0,0 +1,14 @@
+package services;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class InputService extends Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		
+		return null;
+	}
+
+}
